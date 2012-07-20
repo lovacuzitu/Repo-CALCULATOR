590fa584510fc29d15133eecc47aba5ce25d49dc
Dejan Jankovic <dejo.janko@gmail.com>
2012-07-20 16:54:21 +0200

    Ceo projekat

---
+++ Digitron/Digitron/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Digitron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sedam = new System.Windows.Forms.Button();
+            this.osam = new System.Windows.Forms.Button();
+            this.cetiri = new System.Windows.Forms.Button();
+            this.jedan = new System.Windows.Forms.Button();
+            this.pet = new System.Windows.Forms.Button();
+            this.devet = new System.Windows.Forms.Button();
+            this.sest = new System.Windows.Forms.Button();
+            this.dva = new System.Windows.Forms.Button();
+            this.tri = new System.Windows.Forms.Button();
+            this.nula = new System.Windows.Forms.Button();
+            this.tacka = new System.Windows.Forms.Button();
+            this.jednako = new System.Windows.Forms.Button();
+            this.deljenje = new System.Windows.Forms.Button();
+            this.mnozenje = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // sedam
+            // 
+            this.sedam.Location = new System.Drawing.Point(12, 60);
+            this.sedam.Name = "sedam";
+            this.sedam.Size = new System.Drawing.Size(60, 41);
+            this.sedam.TabIndex = 1;
+            this.sedam.Text = "7";
+            this.sedam.UseVisualStyleBackColor = true;
+            this.sedam.Click += new System.EventHandler(this.sedam_Click);
+            // 
+            // osam
+            // 
+            this.osam.Location = new System.Drawing.Point(78, 60);
+            this.osam.Name = "osam";
+            this.osam.Size = new System.Drawing.Size(60, 41);
+            this.osam.TabIndex = 2;
+            this.osam.Text = "8";
+            this.osam.UseVisualStyleBackColor = true;
+            this.osam.Click += new System.EventHandler(this.osam_Click);
+            // 
+            // cetiri
+            // 
+            this.cetiri.Location = new System.Drawing.Point(12, 107);
+            this.cetiri.Name = "cetiri";
+            this.cetiri.Size = new System.Drawing.Size(60, 41);
+            this.cetiri.TabIndex = 3;
+            this.cetiri.Text = "4";
+            this.cetiri.UseVisualStyleBackColor = true;
+            this.cetiri.Click += new System.EventHandler(this.cetiri_Click);
+            // 
+            // jedan
+            // 
+            this.jedan.Location = new System.Drawing.Point(12, 154);
+            this.jedan.Name = "jedan";
+            this.jedan.Size = new System.Drawing.Size(60, 41);
+            this.jedan.TabIndex = 4;
+            this.jedan.Text = "1";
+            this.jedan.UseVisualStyleBackColor = true;
+            this.jedan.Click += new System.EventHandler(this.jedan_Click);
+            // 
+            // pet
+            // 
+            this.pet.Location = new System.Drawing.Point(78, 106);
+            this.pet.Name = "pet";
+            this.pet.Size = new System.Drawing.Size(60, 41);
+            this.pet.TabIndex = 5;
+            this.pet.Text = "5";
+            this.pet.UseVisualStyleBackColor = true;
+            this.pet.Click += new System.EventHandler(this.pet_Click);
+            // 
+            // devet
+            // 
+            this.devet.Location = new System.Drawing.Point(144, 60);
+            this.devet.Name = "devet";
+            this.devet.Size = new System.Drawing.Size(65, 41);
+            this.devet.TabIndex = 6;
+            this.devet.Text = "9";
+            this.devet.UseVisualStyleBackColor = true;
+            this.devet.Click += new System.EventHandler(this.devet_Click);
+            // 
+            // sest
+            // 
+            this.sest.Location = new System.Drawing.Point(144, 106);
+            this.sest.Name = "sest";
+            this.sest.Size = new System.Drawing.Size(65, 41);
+            this.sest.TabIndex = 7;
+            this.sest.Text = "6";
+            this.sest.UseVisualStyleBackColor = true;
+            this.sest.Click += new System.EventHandler(this.sest_Click);
+            // 
+            // dva
+            // 
+            this.dva.Location = new System.Drawing.Point(78, 154);
+            this.dva.Name = "dva";
+            this.dva.Size = new System.Drawing.Size(60, 41);
+            this.dva.TabIndex = 8;
+            this.dva.Text = "2";
+            this.dva.UseVisualStyleBackColor = true;
+            this.dva.Click += new System.EventHandler(this.dva_Click);
+            // 
+            // tri
+            // 
+            this.tri.Location = new System.Drawing.Point(144, 153);
+            this.tri.Name = "tri";
+            this.tri.Size = new System.Drawing.Size(65, 41);
+            this.tri.TabIndex = 9;
+            this.tri.Text = "3";
+            this.tri.UseVisualStyleBackColor = true;
+            this.tri.Click += new System.EventHandler(this.tri_Click);
+            // 
+            // nula
+            // 
+            this.nula.Location = new System.Drawing.Point(12, 200);
+            this.nula.Name = "nula";
+            this.nula.Size = new System.Drawing.Size(60, 41);
+            this.nula.TabIndex = 10;
+            this.nula.Text = "0";
+            this.nula.UseVisualStyleBackColor = true;
+            this.nula.Click += new System.EventHandler(this.nula_Click);
+            // 
+            // tacka
+            // 
+            this.tacka.Location = new System.Drawing.Point(78, 200);
+            this.tacka.Name = "tacka";
+            this.tacka.Size = new System.Drawing.Size(60, 41);
+            this.tacka.TabIndex = 11;
+            this.tacka.Text = ".";
+            this.tacka.UseVisualStyleBackColor = true;
+            this.tacka.Click += new System.EventHandler(this.tacka_Click);
+            // 
+            // jednako
+            // 
+            this.jednako.Location = new System.Drawing.Point(144, 200);
+            this.jednako.Name = "jednako";
+            this.jednako.Size = new System.Drawing.Size(65, 41);
+            this.jednako.TabIndex = 12;
+            this.jednako.Text = "=";
+            this.jednako.UseVisualStyleBackColor = true;
+            this.jednako.Click += new System.EventHandler(this.jednako_Click);
+            // 
+            // deljenje
+            // 
+            this.deljenje.Location = new System.Drawing.Point(215, 60);
+            this.deljenje.Name = "deljenje";
+            this.deljenje.Size = new System.Drawing.Size(57, 41);
+            this.deljenje.TabIndex = 13;
+            this.deljenje.Text = "/";
+            this.deljenje.UseVisualStyleBackColor = true;
+            this.deljenje.Click += new System.EventHandler(this.deljenje_Click);
+            // 
+            // mnozenje
+            // 
+            this.mnozenje.Location = new System.Drawing.Point(215, 106);
+            this.mnozenje.Name = "mnozenje";
+            this.mnozenje.Size = new System.Drawing.Size(57, 41);
+            this.mnozenje.TabIndex = 14;
+            this.mnozenje.Text = "*";
+            this.mnozenje.UseVisualStyleBackColor = true;
+            this.mnozenje.Click += new System.EventHandler(this.mnozenje_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(215, 153);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(57, 41);
+            this.minus.TabIndex = 15;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(215, 200);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(57, 41);
+            this.plus.TabIndex = 16;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 25);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 25);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "+/-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.mnozenje);
+            this.Controls.Add(this.deljenje);
+            this.Controls.Add(this.jednako);
+            this.Controls.Add(this.tacka);
+            this.Controls.Add(this.nula);
+            this.Controls.Add(this.tri);
+            this.Controls.Add(this.dva);
+            this.Controls.Add(this.sest);
+            this.Controls.Add(this.devet);
+            this.Controls.Add(this.pet);
+            this.Controls.Add(this.jedan);
+            this.Controls.Add(this.cetiri);
+            this.Controls.Add(this.osam);
+            this.Controls.Add(this.sedam);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sedam;
+        private System.Windows.Forms.Button osam;
+        private System.Windows.Forms.Button cetiri;
+        private System.Windows.Forms.Button jedan;
+        private System.Windows.Forms.Button pet;
+        private System.Windows.Forms.Button devet;
+        private System.Windows.Forms.Button sest;
+        private System.Windows.Forms.Button dva;
+        private System.Windows.Forms.Button tri;
+        private System.Windows.Forms.Button nula;
+        private System.Windows.Forms.Button tacka;
+        private System.Windows.Forms.Button jednako;
+        private System.Windows.Forms.Button deljenje;
+        private System.Windows.Forms.Button mnozenje;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Digitron/Digitron/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Digitron
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        
+        bool daLiJeNula=true;     // proverava da li je 0 na pocetku linije u textBoxu
+        bool znaakPlus = false;   // naredna 3 bool-a ce nam trebati za racunanje vrednosti, kada kliknemo na dugme "="
+        bool znaakMinus = false;
+        bool znakPuta = false;
+        bool znakPodeljeno = false;
+        bool znakJednako = false; // ovaj bool ce nam trebati za resetovanje nakon jednog racunanja
+
+        public void ProveriDaLiJeRacunato() // ova metoda proverava da li je vrseno racunanje
+        {                                   //ako jeste, onda ukucavanjem narednog broja se resetuje textBox i ponovo se vrsi racunanje za  nove brojeve
+            if (znakJednako)
+            {
+                textBox1.Text = "";
+                znakJednako = false;
+            }
+        }
+
+        private void jedan_Click(object sender, EventArgs e)  // nakon sto kliknemo na dugme 1, on uradi sledece
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula) 
+            {
+                textBox1.Text = "1";      // ako je na  pocetku linije u textBoxu nula, on je prelepi sa jedinicom
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "1";         // ako nije bila nula, on samo doda jedinicu
+        }
+
+        private void nula_Click(object sender, EventArgs e)  // kada kliknemo na dugme nula , uradi sledece
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)               // ako je nula na pocetku, ne radi nista u metodi, tj. ne dodaji dalje nule
+                return;
+            else
+                textBox1.Text += "0";     // u suprotnom dodaji koliko hoces
+        }
+
+        private void dva_Click(object sender, EventArgs e) // i za dugme dva, kao i za preostalih 7 dugmica, isto se radi
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "2";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "2";
+        }
+
+        private void tri_Click(object sender, EventArgs e)
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "3";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "3";
+        }
+
+        private void cetiri_Click(object sender, EventArgs e)
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "4";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "4";
+        }
+
+        private void pet_Click(object sender, EventArgs e)
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "5";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "5";
+        }
+
+        private void sest_Click(object sender, EventArgs e)
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "6";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "6";
+        }
+
+        private void sedam_Click(object sender, EventArgs e)
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "7";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "7";
+        }
+
+        private void osam_Click(object sender, EventArgs e)
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "8";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "8";
+        }
+
+        private void devet_Click(object sender, EventArgs e)
+        {
+            ProveriDaLiJeRacunato();
+
+            if (daLiJeNula)
+            {
+                textBox1.Text = "9";
+                daLiJeNula = false;
+            }
+            else
+            textBox1.Text += "9";
+        }
+
+        private void deljenje_Click(object sender, EventArgs e)  // e sad, kada kliknemo na dugme deljenje, izvrsi se sledece
+        {
+            if (textBox1.Text == "") // ukoliko nije unet nijedan broj u textBox, ne zelimo da izvrsacamo operaciju deljenja
+            {
+                return;
+            }
+            else
+            {
+                znakPodeljeno = true;           // postavljamo bool vrednosti na true, sto ce nam trebati u metodi "jednako_Click"
+                textBox1.Tag = textBox1.Text;   // pamtimo vrednost koju smo uneli pre znaka plus
+                textBox1.Text = "";             // a zatim obrisemo sve sto se nalazi u text boxu
+            }
+        }
+
+        private void mnozenje_Click(object sender, EventArgs e) // i za dugme mnozenje radimo isto kao za ugme deljenje, a i za preostala dva dugmeta
+        {
+            if (textBox1.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                znakPuta = true;
+                textBox1.Tag = textBox1.Text;  
+                textBox1.Text = "";            
+            }
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                znaakMinus = true;
+                textBox1.Tag = textBox1.Text;  
+                textBox1.Text = "";          
+            }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                znaakPlus = true;  
+                textBox1.Tag = textBox1.Text; 
+                textBox1.Text = "";             
+            }
+        }
+                       
+        private void jednako_Click(object sender, EventArgs e)  // najbitnija metoda 
+        {
+            znakJednako = true;
+
+            if (znaakPlus)
+            {
+                decimal decimalniBroj = Convert.ToDecimal(textBox1.Tag) + Convert.ToDecimal(textBox1.Text); // sabiramo dve vrednosti...konvertujemo u decimalni, jer ce nam vecina rezultata upravo biti decimalan broj
+                textBox1.Text = decimalniBroj.ToString(); // rezultat prikazujem u textBoxu, a posto je u pitanju broj, moramo da stavimo metodu ToString
+            }
+
+            if (znaakMinus)
+            {
+                decimal decimalniBroj = Convert.ToDecimal(textBox1.Tag) - Convert.ToDecimal(textBox1.Text); // oduzimamo dve vrednosti
+                textBox1.Text = decimalniBroj.ToString(); // da bi ga prikazao u text boxu 
+            }
+
+            if (znakPuta)
+            {
+                decimal decimalniBroj = Convert.ToDecimal(textBox1.Tag) * Convert.ToDecimal(textBox1.Text); // sabiramo dve vrednosti
+                textBox1.Text = decimalniBroj.ToString(); // da bi ga prikazao u text boxu 
+            }
+
+            if (znakPodeljeno)
+            {
+                decimal decimalniBroj = Convert.ToDecimal(textBox1.Tag) / Convert.ToDecimal(textBox1.Text); // sabiramo dve vrednosti
+                textBox1.Text = decimalniBroj.ToString(); // da bi ga prikazao u text boxu 
+            }
+
+           
+        }
+                         
+        private void tacka_Click(object sender, EventArgs e)  // metoda za decimalne brojeve
+        {
+            if (textBox1.Text.Contains("."))   //ako broj vec sadrzi decimalnu tacku, iskoci iz metode (ne zelimo dve decimalne tacke)
+            {                                  
+                return;                            
+            }
+            else
+                textBox1.Text += ".";           // u suprotnom stavi decimalnu tacku
+        }
+
+        private void button1_Click(object sender, EventArgs e)          // dugme CLEAR
+        {
+            textBox1.Text = "0";                                        // postavi textBox na nulu
+            daLiJeNula = true;                                          // i bool vrednost resetuj na true, sto je bitno za naredna racunanja
+            znaakPlus = znaakMinus = znakPodeljeno = znakPuta = znakJednako= false; // resetujemo svako dugme na pocetnu vrednost
+        }
+
+        private void button2_Click(object sender, EventArgs e) // dugme za pozitivne i negativne brojeve
+        {
+            if (textBox1.Text.Contains("-"))                   // ako je broj vec negativan, ti mu na klik dugmeta obrisi znak minus
+            {
+                textBox1.Text = textBox1.Text.Remove(0, 1);
+            }
+            else
+                textBox1.Text = "-" + textBox1.Text;            // a ako nije bio negativan, ti mu dodaj znak minus
+        }
+
+
+       
+     
+    }
+}
